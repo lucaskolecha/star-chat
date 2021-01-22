cb9c896dcd4ec4bf58db5c8103422a5f1577e4e5
lucaskolecha <lskolecha@gmail.com>
2021-01-22 17:18:20 -0300

    Correçao css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,25 +3,25 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: ["./src/*/.{js,ts,jsx,tsx}"],
+  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {},
   variants: {},
   plugins: [
     function ({ addComponents }) {
       addComponents({
-        ".container": {
-          width: "100%",
-          "@screen sm": {
-            maxWidth: "640px",
+        '.container': {
+          width: '100%',
+          '@screen sm': {
+            maxWidth: '640px',
           },
-          "@screen md": {
-            maxWidth: "768px",
+          '@screen md': {
+            maxWidth: '768px',
           },
-          "@screen lg": {
-            maxWidth: "1024px",
+          '@screen lg': {
+            maxWidth: '1024px',
           },
-          "@screen xl": {
-            maxWidth: "1530px",
+          '@screen xl': {
+            maxWidth: '1530px',
           },
         },
       });
