21964845ce3668506ec30cf72af63c3d9dca1621
lucaskolecha <lskolecha@gmail.com>
2021-01-25 08:19:58 -0300

    Correçao de importaçao ....

--- src/components/Chat/components/Messages/components/Message/index.tsx
+++ src/components/Chat/components/Messages/components/Message/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Cookie } from '../../../../../../helpers/namespaces/cookie';
 import moment from '../../../../../../helpers/moment';
+import { Cookie } from '../../../../../../helpers/namespaces/cookie';
 
 interface Props {
   data: any;
